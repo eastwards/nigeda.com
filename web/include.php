dbcb642e605b857fabe714dbb85cb2b50bb4c83d
eastwards <xtvj007@gmail.com>
2016-05-31 18:24:07 +0800

    change file path

--- web/include.php
+++ web/include.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * 项目入口
+ * Spring out include file.
+ * 方便其他项目调用本项目框架功能代码。(同Spring框架不适用本方式)
  */
 set_time_limit(0);                                          //设置超时时间
 $webDir = dirname(__FILE__);                                //获取当前路径
@@ -8,8 +9,8 @@ $libDir = realpath($webDir . '/../Spring');
       
 define('WebDir', $webDir);                                  //定义项目路径
 define('ActionDir', $webDir.'/App/Console');                //定义控制器存放路径
-require($libDir.'/Spring.php');                             //载入框架入口文件
-require(ConfigDir.'/app.config.php');                       //载入应用全局配置
+require_once($libDir.'/Spring.php');                             //载入框架入口文件
+require_once(ConfigDir.'/app.config.php');                       //载入应用全局配置
 
 return Spring::out();
 
